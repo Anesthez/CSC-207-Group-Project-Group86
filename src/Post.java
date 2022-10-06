022b9921767581afa5ec64caf7639223bb076d47
eric-qli <92590396+eric-qli@users.noreply.github.com>
2022-10-06 15:48:47 -0400

    post and manager

--- src/Post.java
+++ src/Post.java
@@ -1,19 +1,37 @@
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 public class Post {
 
+
+    private String postTitle;
     private int userId;
     private Timestamp timestamp;
     private int postId;
     private String content;
     private int views;
+    private int numLikes;
+    private ArrayList<User> userLiked;
 
-    public Post(int userId, int postId, String content) {
+
+    public Post(String postTitle, int userId, int postId, String content) {
+        this.postTitle = postTitle;
         this.userId = userId;
         this.timestamp = new Timestamp(System.currentTimeMillis());
         this.postId = postId;
         this.content = content;
         this.views = 0;
+        this.num_likes = 0;
+        this.user_liked = new ArrayList<User>();
+    }
+
+
+    public String getPostTitle(){
+        return this.postTitle;
+    }
+
+    public void setPostTitle(String postTitle){
+        this.postTitle = postTitle;
     }
 
     public int getUserId() {
@@ -42,5 +60,29 @@ public class Post {
 
     public void addViews(){
         this.views ++;
+    }
+
+    public String likes(User: user){
+        for (User u: userLiked){
+            int pos = 0; // position of the arrayList
+            /**
+             * change the fucntion name when user class is done
+             */
+            if (u.getUserId() == user.getUserId){
+                numLikes--;
+                userLiked.remove(pos);
+                return null;
+            }
+            pos++;
+        }
+
+        numLikes++;
+        userLiked.add(user);
+        return null;
+        /**
+         * first check if the user already liked the post
+         * case 1 liked: remove the like and remove the user from liked list
+         * case 2 havent liked: add a like and add the user to the liked list
+         */
     }
 }
---
+++ src/postManager.java
@@ -0,0 +1,38 @@
+public class postManager {
+    public void addPost(int userId, int postId, String content){
+        Post post = new Post(userId, postId, content);
+    }
+
+    public Post getPostFromId(int postId){
+        /**
+         * pull from the csv file
+         */
+    }
+
+    public void addComment(Comment comment){
+        /**
+         * idk from where but this is easy and will come back to it
+         */
+    }
+
+    public void deleteComment(Comment comment){
+        /**
+         * same as addComment
+         */
+    }
+
+    public Post searchPost(){
+
+    }
+
+
+    /**
+     * questions:
+     * 1. how to storage comment
+     * 2. delete by comment or commentid
+     * 3. delete from the file or hide from the post
+     * 4. if this blog is text based then there should be a title for each post
+     * 5. search method: search engine? search by post id? should id be private?
+     *
+     */
+}
