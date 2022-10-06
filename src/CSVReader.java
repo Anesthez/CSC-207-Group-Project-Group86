c18c398ef9a4b9ee9851efb18a4bf4ae68e23c6d
KevinZ <stardustinglove@gmail.com>
2022-10-06 18:12:19 -0400

    Created CSVReader.java

---
+++ src/CSVReader.java
@@ -0,0 +1,35 @@
+/*
+ * This class is for reading scv files.
+ *
+ *
+ *
+ */
+
+
+import java.util.List;
+
+public class CSVReader {
+    public void postsReader() {
+        //TODO to be implemented
+    }
+
+    public void postsLikedReader() {
+        //TODO to be implemented
+    }
+
+    public void usersReader() {
+        //TODO to be implemented
+    }
+
+    public void friendsReader() {
+        //TODO to be implemented
+    }
+
+    public void chatsReader() {
+        //TODO to be implemented
+    }
+
+    public void commentsReader() {
+        //TODO to be implemented
+    }
+}
