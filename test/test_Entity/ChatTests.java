ef7a0e7d7eefce6389cb1a72afb8c8ce683c2501
StarryDust-02 <starrydust.02@icloud.com>
2022-10-17 15:22:14 -0400

    ChatTest partial complete

--- test/test_Entity/ChatTests.java
+++ test/test_Entity/ChatTests.java
@@ -2,9 +2,6 @@ package test_Entity;
 
 import Entity.Chat;
 import org.junit.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
@@ -19,15 +16,33 @@ public class ChatTests {
     public void tearDown() {
     }
 
+    Chat actual = new Chat(1, 123, 456, "Hello!", "2022");
+    int[] intExpected = {1, 123, 456};
+    String[] strExpected = {"Hello!", "2022"};
+
     @Test(timeout = 500)
-    public void TestInit(){
-        Chat actual = new Chat(1, 123, 456, "Hello!", "2022");
-        int[] intExpected = {1, 123, 456};
-        String[] strExpected = {"Hello!", "2022"};
-        assertEquals("There is error in Chat.id!", actual.getId(), intExpected[0]);
-        assertEquals("There is error in Chat.user1_id!", actual.getSender_id(), intExpected[1]);
-        assertEquals("There is error in Chat.user2_id!", actual.getReceiver_id(), intExpected[2]);
-        assertEquals("There is error in Chat.content!", actual.getContent(), strExpected[0]);
-        assertEquals("There is error in Chat.time!", actual.getTime(), strExpected[1]);
+    public void TestGetID(){
+        assertEquals("There is error in Chat.id!", intExpected[0], actual.getId());
     }
+
+    @Test(timeout = 500)
+    public void TestGetSender(){
+        assertEquals("There is error in Chat.user1_id!", intExpected[1], actual.getSender_id());
+    }
+
+    @Test(timeout = 500)
+    public void TestGetReceiver(){
+        assertEquals("There is error in Chat.user2_id!", intExpected[2], actual.getReceiver_id());
+    }
+
+    @Test(timeout = 500)
+    public void TestContent(){
+        assertEquals("There is error in Chat.content!", strExpected[0], actual.getContent());
+    }
+
+    @Test(timeout = 500)
+    public void TestTime(){
+        assertEquals("There is error in Chat.time!", strExpected[1], actual.getTime());
+    }
+
 }
