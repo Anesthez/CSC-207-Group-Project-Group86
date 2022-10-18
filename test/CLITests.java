f08f7df73cf438f550ccb2a62ee889b2ff270149
StarryDust-02 <starrydust.02@icloud.com>
2022-10-18 19:51:45 -0400

    CLITests.java for running instance.

---
+++ test/CLITests.java
@@ -0,0 +1,20 @@
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CLITests {
+    static CLI actual;
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testValidInput() {
+        actual = new CLI();
+    }
+}
