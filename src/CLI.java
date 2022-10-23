3f6390705e884339a5897d72361fcb8b2888597d
woooooooOT <78742794+woooooooOT@users.noreply.github.com>
2022-10-23 18:49:31 -0400

    Add CLI for: friends(add/delete), comments(add/delete), post(partial "show")

--- src/CLI.java
+++ src/CLI.java
@@ -11,6 +11,7 @@ public class CLI {
     private int userid;
 
     private final csvInterface csvInteract = new csvInterface();
+
     public void loginInterface() throws IOException {
         Scanner input = new Scanner(System.in);
 
@@ -56,22 +57,26 @@ public class CLI {
         }
     }
 
-    public void generalInterface() throws IOException{
-        Scanner input=new Scanner(System.in);
+    public void generalInterface() throws IOException {
+        Scanner input = new Scanner(System.in);
         csvInterface csvInteract = new csvInterface();
         boolean flag = true;
-        while(flag){
+        while (flag) {
             System.out.print(username);
             String userInput = input.nextLine();
-            if (userInput.length() < 5){
+            if (userInput.length() < 5) {
                 System.out.println("unknown command");
-            } else if (userInput.substring(0, 5).equals("/post")){
+            } else if (userInput.substring(0, 5).equals("/post")) {
                 String[] inputLines = userInput.substring(6).split("-");
                 postInterface(inputLines);
-            }else if (userInput.substring(0, 5).equals("/exit")) {
-                flag = false;
-            }else if (userInput.substring(0, 8).equals("/comment")){
+            } else if (userInput.substring(0, 8).equals("/friends")) {
                 String[] inputLines = userInput.substring(9).split("-");
+                friendsInterface(inputLines);
+            } else if (userInput.substring(0, 5).equals("/exit")) {
+                flag = false;
+            } else if (userInput.substring(0, 8).equals("/comment")) {
+                String[] inputLines = userInput.substring(9).split("-");
+                commentInterface(inputLines);
             } else {
                 System.out.println("unknown command");
             }
@@ -83,9 +88,69 @@ public class CLI {
         Map<Integer, ArrayList<Integer>> postLiked =
                 csvInteract.postsLikedReader("database/post_liked.csv");
         PostManager postManager = new PostManager(posts, postLiked);
-        if (inputLines.length == 3){
+        if (inputLines.length == 3) {
             postManager.addPost(inputLines[1], userid, inputLines[2]);
             csvInteract.postsWriter("database/post.csv", posts);
+        } else if (inputLines[1].equals("show")) {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(postManager.showPost(i));
+            }
+            //TODO complete show post after discussion
+            //TODO question: show 5 recent posts or 5 recent posts from user
+
+        }
+    }
+
+    public void friendsInterface(String[] inputLines) throws IOException {
+        csvInterface csvInteract = new csvInterface();
+        Map<Integer, User> users = csvInteract.usersReader("database/user.csv");
+        Map<Integer, ArrayList<Integer>> friends =
+                csvInteract.friendsReader("database/friends.csv");
+        if (inputLines[1].equals("add")) {
+            int friendid = Integer.parseInt(inputLines[2]);
+            if (users.containsKey(friendid)) {
+                friends.get(userid).add(friendid);
+                csvInteract.friendsWriter("database/friends.csv", friends);
+            } else {
+                System.out.println("user does not exist");
+            }
+        } else if (inputLines[1].equals("remove")) {
+            int friendid = Integer.parseInt(inputLines[2]);
+            if (users.containsKey(friendid)) {
+                friends.get(userid).remove(friendid);
+                csvInteract.friendsWriter("database/friends.csv", friends);
+            } else {
+                System.out.println("user does not exist");
+            }
+        } else {
+            System.out.println("unknown command");
+        }
+    }
+
+    public void commentInterface(String[] inputLines) throws IOException {
+        csvInterface csvInteract = new csvInterface();
+        Map<Integer, Post> posts = csvInteract.postsReader("database/post.csv");
+        Map<Integer, Comment> comments = csvInteract.commentsReader("database/comments.csv");
+        if (inputLines[1].equals("add")) {
+            int postid = Integer.parseInt(inputLines[2]);
+            if (posts.containsKey(postid)) {
+                CommentManager commentManager = new CommentManager();
+                commentManager.addComment(userid, inputLines[3]); //TODO: no postid in args?
+                csvInteract.commentsWriter(comments, "database/comments.csv");
+            } else {
+                System.out.println("post does not exist");
+            }
+        } else if (inputLines[1].equals("delete")) {
+            int commentid = Integer.parseInt(inputLines[2]);
+            if (comments.containsKey(commentid)) {
+                CommentManager commentManager = new CommentManager();
+                commentManager.deleteComment(commentid);
+                csvInteract.commentsWriter(comments, "database/comments.csv");
+            } else {
+                System.out.println("comment does not exist");
+            }
+        } else {
+            System.out.println("unknown command");
         }
     }
 
