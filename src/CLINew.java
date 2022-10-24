6885715a5a3b09fdc46b1edf939cd42fe4bbc6ca
StarryDust-02 <starrydust.02@icloud.com>
2022-10-24 15:16:54 -0400

    CLI recreate attempt.

--- src/CLINew.java
+++ src/CLINew.java
@@ -1,2 +1,17 @@
+import Entity.*;
+import Interface.*;
+import repositories.*;
+
+import java.io.*;
+import java.util.*;
+
+/* CLI rework attempt by Tianyu Li @StarryDust-02
+
+ */
 public class CLINew {
+    private String username;
+    private int userid;
+
+    private final csvInterface csvInteract = new csvInterface();
+
 }
