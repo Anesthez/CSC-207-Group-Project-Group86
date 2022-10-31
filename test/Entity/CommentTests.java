f1df4a236e13ef09763ad6a2b8957b2da321016e
LemengDai <51035918+LemengDai@users.noreply.github.com>
2022-10-31 17:20:30 -0400

    Add Tests for Comment

--- test/Entity/CommentTests.java
+++ test/Entity/CommentTests.java
@@ -4,14 +4,57 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 /*
- * Author: Tianyu Li
+ * Author: Tianyu Li, LemengDai
  */
 public class CommentTests {
+    int[] intExpected = {5, 15, 0, 100};
+    static Comment  actual;
+    String[] strExpected = {"Nice Picture!", "2022.05.15"};
+
     @BeforeClass
     public static void setUp() {
+        actual = new Comment(5, 15, "Nice Picture!", "2022.05.15");
     }
 
     @After
     public void tearDown() {
     }
+
+    @Test(timeout = 500)
+    public void TestGetUserID() {
+        assertEquals("There is error in Comment.userId",
+                intExpected[0], actual.getUserId());
+    }
+
+    @Test(timeout = 500)
+    public void TestGetID(){
+        assertEquals("There is error in Comment.id!",
+                intExpected[1], actual.getId());
+    }
+    @Test(timeout = 500)
+    public void TestContent(){
+        assertEquals("There is error in Comment.content!",
+                strExpected[0], actual.getContent());
+    }
+
+    @Test(timeout = 500)
+    public void TestTime(){
+        assertEquals("There is error in Comment.time!",
+                strExpected[1], actual.getTime());
+    }
+
+    @Test(timeout = 500)
+    public void TestGetViews() {
+        actual = new Comment(5, 15, "Nice Picture!", "2022.05.15");
+        assertEquals("There is error in Comment.getViews!", intExpected[2], actual.getViews());
+
+    }
+
+    @Test(timeout = 500)
+    public void TestSetViews() {
+        actual.setViews(100);
+        assertEquals("There is error in Comment.setViews!", intExpected[3], actual.getViews());
+    }
 }
+
+
