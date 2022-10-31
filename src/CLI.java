dd8d371b9e053426b1af894f84525752b139a2d7
DominicGu <jiahao.gu@mail.utoronto.ca>
2022-10-31 15:38:35 -0400

    add chat case to generalInterface

--- src/CLI.java
+++ src/CLI.java
@@ -60,6 +60,7 @@ public class CLI {
         }
     }
 
+    //Author: Jiahao Gu
     public void chatInterface(String[] inputLines) throws IOException {
         csvInterface csvInteract = new csvInterface();
         Map<Integer, User> users = csvInteract.usersReader("database/user.csv");
@@ -67,67 +68,69 @@ public class CLI {
         ChatManager chatManager = new ChatManager(chats);
 
         // use "/chat-add-receiver's id-content" to send a message
-        // TODO consider using switch to simplify the code -- Tianyu Li
-        if(inputLines[1].equals("add")){
-            int receiver_id = Integer.parseInt(inputLines[2]);
-            if (users.containsKey(receiver_id)) {
-                chatManager.addChat(userid, receiver_id, inputLines[3]);
-                csvInteract.chatsWriter(chats, "database/chat.csv");
-                System.out.println("message sent");
-            } else {
-                System.out.println("user does not exist");
+        switch (inputLines[1]) {
+            case "add": {
+                int receiver_id = Integer.parseInt(inputLines[2]);
+                if (users.containsKey(receiver_id)) {
+                    chatManager.addChat(userid, receiver_id, inputLines[3]);
+                    csvInteract.chatsWriter(chats, "database/chat.csv");
+                    System.out.println("message sent");
+                } else {
+                    System.out.println("user does not exist");
+                }
             }
-        }
 
-        // use "/chat-delete-chatid" to delete a message
-        else if (inputLines[1].equals("delete")){
-            int chatid = Integer.parseInt(inputLines[2]);
-            if (chats.containsKey(chatid)) {
-                chatManager.deleteChat(chatid);
-                csvInteract.chatsWriter(chats, "database/chat.csv");
-                System.out.println("message deleted");
-            }
-            else{
-                System.out.println("chat does not exist");
-            }
-        }
 
-        // use "/chat-showid-receiver's id-sent time(yyyy.MM.dd hh:mm:ss)" to find the id of a message
-        else if (inputLines[1].equals("showid")){
-            int chatid = chatManager.getIdByUserAndTime(userid, Integer.parseInt(inputLines[2]), inputLines[3]);
-            if (chatid != 0){
-                System.out.println("chat id is " + chatid);
+            // use "/chat-delete-chatid" to delete a message
+            case "delete": {
+                int chatid = Integer.parseInt(inputLines[2]);
+                if (chats.containsKey(chatid)) {
+                    chatManager.deleteChat(chatid);
+                    csvInteract.chatsWriter(chats, "database/chat.csv");
+                    System.out.println("message deleted");
+                } else {
+                    System.out.println("chat does not exist");
+                }
+                break;
             }
-            else{
-                System.out.println("chat does not exist");
-            }
-        }
 
-        // use "/chat-show-receiver's id" to show all messages sent between two people
-        else if(inputLines[1].equals("show")){
-            int receiver_id = Integer.parseInt(inputLines[2]);
-            if (users.containsKey(receiver_id)) {
-                List<Chat> chatlist = new ArrayList<>();
-                for (int id: chats.keySet()){
-                    if (chats.get(id).getSender_id() == userid && chats.get(id).getReceiver_id() == receiver_id ||
-                            chats.get(id).getSender_id() == receiver_id && chats.get(id).getReceiver_id() == userid){
-                        chatlist.add(chats.get(id));
+
+            // use "/chat-showid-receiver's id-sent time(yyyy.MM.dd hh:mm:ss)" to find the id of a message
+            case "showid": {
+                int chatid = chatManager.getIdByUserAndTime(userid, Integer.parseInt(inputLines[2]), inputLines[3]);
+                if (chatid != 0) {
+                    System.out.println("chat id is " + chatid);
+                } else {
+                    System.out.println("chat does not exist");
+                }
+                break;
+            }
+
+
+            // use "/chat-show-receiver's id" to show all messages sent between two people
+            case "show": {
+                int receiver_id = Integer.parseInt(inputLines[2]);
+                if (users.containsKey(receiver_id)) {
+                    List<Chat> chatlist = new ArrayList<>();
+                    for (int id : chats.keySet()) {
+                        if (chats.get(id).getSender_id() == userid && chats.get(id).getReceiver_id() == receiver_id ||
+                                chats.get(id).getSender_id() == receiver_id && chats.get(id).getReceiver_id() == userid) {
+                            chatlist.add(chats.get(id));
+                        }
                     }
+                    if (chatlist.isEmpty()) {
+                        System.out.println("No message found");
+                    }
+                    chatlist.sort(null);
+                    for (Chat c : chatlist) {
+                        c.printChat();
+                    }
+                } else {
+                    System.out.println("user does not exist");
                 }
-                if (chatlist.isEmpty()){
-                    System.out.println("No message found");
-                }
-                chatlist.sort(null);
-                for (Chat c: chatlist){
-                    c.printChat();
-                }
+                break;
             }
-            else {
-                System.out.println("user does not exist");
-            }
-        }
-        else {
-            System.out.println("unknown command");
+            default: System.out.println("unknown command");
         }
     }
 
