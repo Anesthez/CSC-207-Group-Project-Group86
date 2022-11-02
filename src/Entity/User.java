98b5e7066e6226714ad45e03b28795fccffe7fe8
Kevin Wu <kevintqj@gmail.com>
2022-11-02 13:57:25 -0400

    Update documentation for User.java

--- src/Entity/User.java
+++ src/Entity/User.java
@@ -5,8 +5,14 @@ import inputboundary.Timeable;
 
 import java.sql.Timestamp;
 /**
- * Author: Kevin WU
- * Modified by: Yufei Chen
+ * <p>The user object is the profile for each user. The object implements the interfaces of Searchable and Timeable</p>
+ *
+ * <p>To initialize a user object, we need the id for the user, type of user,
+ * username, password, and the time of user creation</p>
+ *
+ * @implNote the time of user creation for the class is not to be altered, thus set as final.
+ * @Author: Kevin WU
+ * @Modifiedby: Yufei Chen
  */
 public class User implements Searchable, Timeable {
     private int id;
