21ba8d1b01bc6670b4857dcbf6b059c3a00c82be
StarryDust-02 <starrydust.02@icloud.com>
2022-11-08 17:19:21 -0500

    HotPostScreen complete >_<

--- src/Layer4/UI/Screens/HotPostScreen.java
+++ src/Layer4/UI/Screens/HotPostScreen.java
@@ -1,9 +1,14 @@
-//package Layer4.UI.Screens;
-//import javax.swing.*;
-//import Layer2.UseCases.*;
-//public class HotPostScreen {
-//
-//    public HotPostScreen(){
-//        JTextArea =
-//    }
-//}
+package Layer4.UI.Screens;
+import javax.swing.*;
+
+import Layer1.Entity.Post;
+import Layer2.UseCases.*;
+import Layer4.UI.Components.*;
+public class HotPostScreen extends JPanel {
+
+    public HotPostScreen(){
+        JButton mostPopular = new JButton("Most Popular");
+        this.setSize(800, 800);
+    }
+
+}
