c249ecbd9f5edc1c9d2dd86852b06da4a8f95149
StarryDust-02 <starrydust.02@icloud.com>
2022-11-08 16:13:27 -0500

    partial

--- src/Layer4/UI/Screens/HotPostScreen.java
+++ src/Layer4/UI/Screens/HotPostScreen.java
@@ -1,9 +1,14 @@
 package Layer4.UI.Screens;
 import javax.swing.*;
+
+import Layer1.Entity.Post;
 import Layer2.UseCases.*;
-public class HotPostScreen {
+import Layer4.UI.Components.*;
+public class HotPostScreen extends JPanel {
 
     public HotPostScreen(){
-        JTextArea =
+        JButton mostPopular = new JButton("Most Popular");
+        this.setSize(800, 800);
     }
+
 }
