b20b8f2d4af86ebfed1963cebf8484e63911f42a
Chen Jiang <cj.jiang@mail.utoronto.ca>
2022-11-09 16:13:10 -0500

    Topic, Topic Factoru, Topic Screen, Topic Use Cases method change

--- src/Layer3/Commands/TopicCommand.java
+++ src/Layer3/Commands/TopicCommand.java
@@ -5,16 +5,49 @@ import Layer4.Interface.csvInterface;
 
 import java.io.IOException;
 import java.util.Map;
-
+/**
+ * <p>
+ *     A class for TopicCommand.
+ * </p>
+ * <p>
+ *     This class is used to let user to command topic and implement topic functions in command. It contains several
+ *     cases: when user wants to view all the topics, they command /show to view all the topics.
+ * </p>
+ * <p>
+ *     This class is part of the Command layer.
+ * </p>
+ *
+ * @Author: Chen Jiang
+ * @Modifiedby: Chen Jiang
+ */
 public class TopicCommand {
     String[] inputlines;
     int userid;
+    /**
+     * <p>
+     *     The constructor of TopicCommand.
+     * </p>
+     * <p>
+     *     This constructor takes in String[] inputlines and int userid and set them to the class.
+     * </p>
+     * @param inputlines the inputlines of the topic
+     * @param userid the userid of the topic
+     */
     public TopicCommand(String[] inputlines, int userid)
     {
         this.inputlines = inputlines;
         this.userid = userid;
     }
 
+    /**
+     * <p>
+     *     The execute method is a method that is used to execute the command.
+     * </p>
+     * <p>
+     *     This method takes in String[] inputlines and int userid and set them to the class.
+     * </p>
+     * @throws IOException
+     */
     public void exact() throws IOException {
         csvInterface csvInterface = new csvInterface();
         Map<Integer, Topic> topicMap = csvInterface.topicsReader("database/topic.csv");
