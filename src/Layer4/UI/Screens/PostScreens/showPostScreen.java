1eba0a750643bd63967ec8650a7bdf5b068c9a7a
Kevin Wu <kevintqj@gmail.com>
2022-11-14 15:36:09 -0500

    Edited post.csv with topic Finished showPostScreen UI

--- src/Layer4/UI/Screens/PostScreens/showPostScreen.java
+++ src/Layer4/UI/Screens/PostScreens/showPostScreen.java
@@ -4,6 +4,8 @@ import Layer3.Controller.PostController;
 import Layer3.Presenter.PostPresenter;
 import Layer4.UI.Components.PlaceButton;
 import Layer4.UI.Components.PlaceLabel;
+import Layer4.UI.Screens.MainScreen;
+import Layer4.UI.Screens.PostScreens.CommentScreens.ShowCommentScreen;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -17,33 +19,40 @@ import java.util.ArrayList;
  * @author Kevin Wu
  */
 public class showPostScreen extends JFrame implements ActionListener {
-    public showPostScreen(int postId) throws Exception {
+    public showPostScreen(int postId, int userId, String name) throws Exception {
         //TODO: change coordinates and sizes
         PostPresenter postPresenter = new PostPresenter();
         ArrayList<Object> labels = new ArrayList<>();
         labels = postPresenter.showPost(postId);
-        this.add(new PlaceLabel().create(50, 0, 50, 50, labels.get(0).toString()));
-        this.add(new PlaceLabel().create(50, 100, 50, 50, labels.get(1).toString()));
-        this.add(new PlaceLabel().create(50, 200, 50, 50, labels.get(2).toString()));
-        this.add(new PlaceLabel().create(50, 300, 50, 50, labels.get(3).toString()));
-        this.add(new PlaceLabel().create(50, 100, 50, 50, labels.get(4).toString()));
+        this.add(new PlaceLabel().create(100, 50, 50, 50, "Title: "));
+        this.add(new PlaceLabel().create(150, 50, 200, 50, labels.get(0).toString()));
+        this.add(new PlaceLabel().create(700, 25, 100, 50, "PostID: "));
+        this.add(new PlaceLabel().create(750, 25, 200, 50, labels.get(1).toString()));
+        this.add(new PlaceLabel().create(350, 50, 200, 50, labels.get(2).toString()));
+        this.add(new PlaceLabel().create(575, 50, 100, 50, "Likes: "));
+        this.add(new PlaceLabel().create(615, 50, 200, 50, labels.get(3).toString()));
+        this.add(new PlaceLabel().create(100, 125, 600, 600, labels.get(4).toString()));
         JButton showCommentsButton = new PlaceButton().create("Show Comments", null,
-                    650, 675, 100, 50);
+                    600, 675, 125, 50);
         showCommentsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO: call show comments screen
+                ShowCommentScreen showCommentScreen = new ShowCommentScreen(userId);
+                showCommentScreen.setVisible(true);
+                dispose();
             }
         });
         this.add(showCommentsButton);
 
 
         JButton backButton = new PlaceButton().create("Back", null,
-                    650, 675, 100, 50);
+                    100, 675, 100, 50);
         backButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO: jump back to the previous screen
+                PostScreen postScreen = new PostScreen(userId, name);
+                postScreen.setVisible(true);
+                dispose();
             }
         });
         this.add(backButton);
