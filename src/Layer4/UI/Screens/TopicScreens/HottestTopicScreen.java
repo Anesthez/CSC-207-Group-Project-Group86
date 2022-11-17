25ddfd92bac4fd7e225535eb382981ac7491e3ca
Chen Jiang <cj.jiang@mail.utoronto.ca>
2022-11-17 17:38:49 -0500

    Topic Screens

---
+++ src/Layer4/UI/Screens/TopicScreens/HottestTopicScreen.java
@@ -0,0 +1,43 @@
+package Layer4.UI.Screens.TopicScreens;
+
+import Layer4.Interface.csvInterface;
+import Layer4.UI.Screens.MainScreen;
+import Model.Request.TopicRequestModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class HottestTopicScreen extends JFrame {
+    public HottestTopicScreen(int userId, String name) throws IOException {
+        JButton back = new JButton("Back");
+        back.setBounds(0, 0, 50, 20);
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainScreen mainScreen = new MainScreen(userId, name);
+                mainScreen.setVisible(true);
+                dispose();
+            }
+        });
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        Map<Integer, TopicRequestModel> topics = new csvInterface().topicsReader("database/topics.csv");
+        //TODO: change the code of controller to get the method
+        TopicController topicController = new TopicController(topics);
+        ArrayList<TopicRequestModel> ht = topicController.getHottestTopic();
+        for (TopicRequestModel t : ht){
+            JLabel label = new JLabel(String.valueOf(t));
+            label.setSize(700, 100);
+            panel.add(label);
+        }
+
+    }
+    public void setVisible(boolean b) {
+    }
+
+}
