7a038b6a42cc567d7f257653001d80da78bb11b9
yufeichen <iufei.Chen@mail.utoronto.ca>
2022-11-22 10:30:16 -0500

    refining the gui

--- src/Layer3/Controller/RegisterController.java
+++ src/Layer3/Controller/RegisterController.java
@@ -5,16 +5,20 @@ import Layer4.Interface.csvInterface;
 import Model.Request.UserRequestModel;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class RegisterController {
     public int register(String username, String password) throws IOException {
         csvInterface csvInteract = new csvInterface();
         Map<Integer, UserRequestModel> users = csvInteract.usersReader("database/user.csv");
+        Map<Integer, ArrayList<Integer>> friends = csvInteract.friendsReader("database/friends.csv");
         UserUsesCases userManager = new UserUsesCases(users);
         int userId = userManager.addUser(username, password);
         if (userId != -1) {
             csvInteract.usersWriter(userManager.getUsers(), "database/user.csv");
+            friends.put(userId, new ArrayList<>(userId));
+            csvInteract.friendsWriter("database/friends.csv", friends);
             return userId;
         }else{
             return -1;
